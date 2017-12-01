420d300ddbef397725c59fe362c031e6a9111014
liuxshang <liuxshang@163.com>
2017-12-01 16:52:05 +0800

    更新

--- samples/httpdemo.py
+++ samples/httpdemo.py
@@ -1,5 +1,12 @@
+import re
 import urllib.request
 
 req = urllib.request
-with req.urlopen("http://www.baidu.com") as f:
-    print(f.read(2000).decode('utf-8'))
+# with req.urlopen("http://www.baidu.com") as f:
+#     print(f.read(2000).decode('utf-8'))
+reg = re.compile('(href="http.*?")')
+with req.urlopen("https://github.com/CysionLiu") as f:
+    content = f.read().decode('utf-8')
+    for s in reg.findall(content):
+        print(s)
+
