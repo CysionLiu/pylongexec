036b563c6b898076b70ec43db479b03e87d2fbb3
liuxshang <liuxshang@163.com>
2017-12-15 18:14:06 +0800

    抓包新浪新闻 国内新闻 基本成型

--- pachongsample/sina_spider.py
+++ pachongsample/sina_spider.py
@@ -28,32 +28,76 @@ def get_simple_lists(url):
             # print(h2, link, time)
 
 
-def get_inner(url):
-    body = get_body(url)
-    article_title = body.select('.main-title')[0].text
-    print(article_title)
-    data_src = body.select('.date-source')[0].select('a')[0].text
-    print(data_src)
-    ctime = body.select('.date')[0].text
-    print(ctime)
-    artciles = body.select('.article')[0].select('p')
-    for a in artciles[:-1]:
-        print(a.text.strip())
-    editor = body.select('.show_author')[0].text.lstrip('责任编辑：')
-    print(editor)
+def get_article_detail(url):
+    try:
+        body = get_body(url)
+        article_title = body.select('.main-title')[0].text
+        data_src = body.select('.date-source')[0].select('a')[0].text
+        ctime = body.select('.date')[0].text
+        artciles = body.select('.article')[0].select('p')
+        article_body = ''
+        for a in artciles[:-1]:
+            article_body += a.text.strip()
+        editor = body.select('.show_author')[0].text.lstrip('责任编辑：')
+        comment_count = get_comment(url)
+        article = {'article_title': article_title, 'article_src': data_src, 'ctime': ctime,
+                   'article_body': '暂不写入', 'editor': editor, 'comment_num': comment_count}
+        # print(article)
+        return article
+    except:
+        return None
 
 
 def get_comment(url):
+    import re
+    rawurl = 'http://comment5.news.sina.com.cn/page/info?version=1&format=json&channel=sh&newsid=comos-{}&group=undefined&compress=0&ie=utf-8&oe=utf-8&page=1&page_size=3&t_size=3&h_size=3&thread=1'
+    re_ids = re.search(r'doc-i(.*).shtml', url)
+    newid = re_ids.group(1)
+    url = rawurl.format(newid)
     body = get_body(url)
-    jsons = json.loads(body.text.lstrip('jsonp_1513242542440(').rstrip(')'))
-    comment_count = jsons['result']['count']['total']
-    print(comment_count)
+    try:
+        jsons = json.loads(body.text)
+        comment_count = jsons['result']['count']['total']
+        return comment_count
+    except:
+        pass
+
+
+def get_article_list(page):
+    urls = []
+    url = 'http://api.roll.news.sina.com.cn/zt_list?' \
+          'channel=news&cat_1=gnxw&cat_2==gdxw1&level==1||=2&show_ext=1&show_all=1&show_num=22' \
+          '&tag=1&format=json&page={}'
+    url = url.format(page)
+    jd = json.loads(get_body(url).text)
+    a_list = jd['result']['data']
+    for a in a_list:
+        urls.append(a['url'])
+        # print(a['title'])
+    return urls
 
 
 if __name__ == '__main__':
-    url = 'http://news.sina.com.cn/china/'
-    url2 = 'http://news.sina.com.cn/o/2017-12-14/doc-ifyptfcn0287301.shtml'
-    url_com = 'http://comment5.news.sina.com.cn/page/info?version=1&format=json&channel=sh&newsid=comos-fypsvkp2763006&group=undefined&compress=0&ie=utf-8&oe=utf-8&page=1&page_size=3&t_size=3&h_size=3&thread=1&callback=jsonp_1513242542440&_=1513242542440'
+    # url = 'http://news.sina.com.cn/china/'
+    # url2 = 'http://news.sina.com.cn/c/2017-12-12/doc-ifypnqvn3401389.shtml'
     # get_simple_lists(url)
-    get_inner(url2)
-    get_comment(url_com)
+    # get_article_detail(url2)
+    # get_comment(url2)
+
+    news_url_list = []
+    news_list = []
+    for i in range(1, 50):
+        temp_L = get_article_list(i)
+        if len(temp_L) < 1:
+            pass
+        news_url_list.extend(temp_L)
+    for n in news_url_list:
+        tmp = get_article_detail(n)
+        if  tmp is not None:
+            news_list.append(tmp)
+            # print(a)
+    import pandas
+
+    df = pandas.DataFrame(news_list)
+    df.to_excel('e:/tmp.xlsx')
+    # print(df)
