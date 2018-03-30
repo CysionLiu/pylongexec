fdb3d0a2ccf8b5f3fda0e14777a8b1897ef22fc7
liuxshang <liuxshang@163.com>
2018-03-30 17:36:10 +0800

    增加个安卓国际化脚本，xml里对应中文信息，excel里是翻译过的，包括英文和日文，调整配置运行后 可以生成对应的英文和日文xml;

---
+++ samples/alterxml.py
@@ -0,0 +1,43 @@
+import xml.dom.minidom
+import xlrd
+# 安卓国际化脚本，xml里对应中文信息，excel里是翻译过的，包括英文和日文，调整配置运行后
+# 可以生成对应的英文和日文xml;
+
+zh,zh_x,en_x,ja_x = [],[],[],[]
+
+def read_xml(path):
+    global zh
+    c = 2;
+    dom = xml.dom.minidom.parse(path)
+    x = dom.getElementsByTagName("string")
+    read_excel()
+    for s in x:
+        for z in zh_x:
+            if s.firstChild.data == z:
+                i = zh_x.index(z)
+                if c==1:
+                    s.firstChild.data = en_x[i]
+                if c==2:
+                    s.firstChild.data = ja_x[i]
+    if c == 1:
+        f = open('../static/target_en.xml', 'w', encoding="utf-8")
+        dom.writexml(f, addindent=' ', newl='', encoding='utf-8')
+        f.close()
+    if c == 2:
+        f2 = open('../static/target_ja.xml', 'w', encoding="utf-8")
+        dom.writexml(f2, addindent=' ', newl='', encoding='utf-8')
+        f2.close()
+
+
+
+
+def read_excel():
+    global zh_x,en_x,ja_x
+    workbook = xlrd.open_workbook('../static/inter.xls')
+    sheet2 = workbook.sheet_by_name('Sheet1')
+    zh_x = sheet2.col_values(0)  # 获取第1列内容
+    en_x = sheet2.col_values(1)  # 获取第2列内容
+    ja_x = sheet2.col_values(2)  # 获取第3列内容
+
+if __name__ == '__main__':
+    read_xml('../static/strings_zh.xml')
---
+++ samples/single.py
@@ -0,0 +1,42 @@
+import re
+import pandas
+
+# coding=utf-8
+import xml.dom.minidom
+
+L = []
+for i in range(3):
+    path = '../static/string' + str(i) + '.xml'
+    # 打开xml文档
+    dom = xml.dom.minidom.parse(path)
+    # 得到文档元素对象
+    root = dom.documentElement
+    cc = dom.getElementsByTagName('string')
+    for c in cc:
+        L.append(c.firstChild.data)
+
+        # print(len(L))
+        # for l in L:
+        # print(l)
+# print("div---")
+s = set(L)
+# print(len(s))
+# for tmp in s:
+#     print(tmp)
+
+
+with open('../static/keywords', encoding='utf-8') as f:
+    result = f.read()
+    Lios = result.split('\n')
+s_ios = set(Lios)
+
+s = s | s_ios
+
+L = list(s)
+L2 = []
+for i in range(len(L)):
+    L2.append(str(i))
+Lios = []
+df = pandas.DataFrame(L, index=L2, columns=['中文'])
+print(df)
+df.to_excel('e:/tmp.xlsx')
