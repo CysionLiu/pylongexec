0c6ee8bfdea729da65cff14bac3d3fc0716185fc
liuxshang <liuxshang@163.com>
2018-05-25 18:08:03 +0800

    练习

--- newdemo/ret.py
+++ newdemo/ret.py
@@ -1,4 +1,5 @@
-import re
+import re,urllib
+from urllib import request
 
 
 def f1():
@@ -10,7 +11,17 @@ def f2():
     s="jglsjr049u0fw3rw3^j4#%#$%^#%rjwflsf0-203i4rwlf!jw2#$@#$@4"
     r=re.compile(r"j\w{1,8}\d")
     print(r.findall(s))
+
+def f3(url):
+    reg=re.compile(r"http\w?://.*\"")
+    with request.urlopen(url) as req:
+        result=req.read().decode()
+        print(result)
+        for u in reg.findall(result):
+            print(u)
+        pass
 if __name__ == '__main__':
     f1()
     f2()
+    f3("https://github.com/features")
     pass
